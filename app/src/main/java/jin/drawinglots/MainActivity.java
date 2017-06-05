f561b497c2e0796094a1e7611b816c4d6f05a2ac
choijinhyun <chjh0902@naver.com>
2017-06-05 17:26:16 +0900

    3st

--- app/src/main/java/jin/drawinglots/MainActivity.java
+++ app/src/main/java/jin/drawinglots/MainActivity.java
@@ -5,8 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
-
 
 import java.util.Random;
 
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     ImageButton imagebutton,imagebutton2,imagebutton3,imagebutton4;
     Button button;
     TextView result_msg;
+    RadioGroup radioGroup;
     int randomNumber;
 
     @Override
@@ -22,7 +24,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       imagebutton  = (ImageButton) findViewById(R.id.imageButton);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+
+        imagebutton  = (ImageButton) findViewById(R.id.imageButton);
         imagebutton2  = (ImageButton) findViewById(R.id.imageButton2);
         imagebutton3  = (ImageButton) findViewById(R.id.imageButton3);
         imagebutton4  = (ImageButton) findViewById(R.id.imageButton4);
@@ -42,26 +46,72 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
-        int n = 0;
+        RadioButton radioButton = (RadioButton) findViewById(R.id.radioButton);
+        RadioButton radioButton2 = (RadioButton) findViewById(R.id.radioButton2);
 
-        switch (v.getId()){
-            case R.id.imageButton: n=1; break;
-            case R.id.imageButton2: n=2; break;
-            case R.id.imageButton3: n=3; break;
-            case R.id.imageButton4: n=4; break;
-            case R.id.button: n=0; break;
+        if(radioButton.isChecked()){
+            int n = 0;
+            imagebutton4.setVisibility(ImageButton.INVISIBLE);
+            switch (v.getId()) {
+                case R.id.imageButton:
+                    n = 1;
+                    break;
+                case R.id.imageButton2:
+                    n = 2;
+                    break;
+                case R.id.imageButton3:
+                    n = 3;
+                    break;
+                case R.id.button:
+                    n = 0;
+                    break;
+            }
+
+            if (n == 0) {
+                randomNumber = new Random().nextInt(3) + 1;
+                button.setVisibility(Button.INVISIBLE);
+                result_msg.setText("두두두두둥~");
+            } else {
+                if (randomNumber == n) {
+                    result_msg.setText("당첨~!");
+                    button.setVisibility(Button.VISIBLE);
+                } else {
+                    result_msg.setText("패스~!");
+                }
+            }
         }
+        else if (radioButton2.isChecked()) {
+            int n = 0;
+            imagebutton4.setVisibility(ImageButton.VISIBLE);
+            switch (v.getId()) {
+                case R.id.imageButton:
+                    n = 1;
+                    break;
+                case R.id.imageButton2:
+                    n = 2;
+                    break;
+                case R.id.imageButton3:
+                    n = 3;
+                    break;
+                case R.id.imageButton4:
+                    n = 4;
+                    break;
+                case R.id.button:
+                    n = 0;
+                    break;
+            }
 
-        if(n==0){
-            randomNumber = new Random().nextInt(4)+1;
-            button.setVisibility(Button.INVISIBLE);
-            result_msg.setText("두두두두둥~");
-        }else{
-            if(randomNumber == n){
-                result_msg.setText("당첨~!");
-                button.setVisibility(Button.VISIBLE);
-            }else{
-                result_msg.setText("패스~!");
+            if (n == 0) {
+                randomNumber = new Random().nextInt(4) + 1;
+                button.setVisibility(Button.INVISIBLE);
+                result_msg.setText("두두두두둥~");
+            } else {
+                if (randomNumber == n) {
+                    result_msg.setText("당첨~!");
+                    button.setVisibility(Button.VISIBLE);
+                } else {
+                    result_msg.setText("패스~!");
+                }
             }
         }
     }
